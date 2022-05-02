ee6657317ad273648903daef5d036f605f6d7d5c
PuQing <me@puqing.work>
2022-05-02 12:45:31 +0800

    🔧 build(upload to pypi):
    when main upload pypi, develop upload test

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="pygeomesh",
-    version="0.1.0.1",
+    version="0.1.0.2",
     author="PuQing",
     author_email="me@puqing.work",
     packages=find_packages(),
