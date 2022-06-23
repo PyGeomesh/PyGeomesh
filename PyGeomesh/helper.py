82605963f4a7161ed31a8a468c461a2e5245d659
PuQing <me@puqing.work>
2022-06-23 23:28:46 +0800

    🐎 ci(.deepsource.toml): code style ci

--- PyGeomesh/helper.py
+++ PyGeomesh/helper.py
@@ -2,16 +2,12 @@ import meshio
 
 
 def read(filename):
-    """
-    Read a mesh from a file.
-    """
+    """Read a mesh from a file."""
     return meshio.read(filename)
 
 
 def extract_points():
-    """
-    Extract the points from a mesh.
-    """
+    """Extract the points from a mesh."""
     import pygmsh
 
     return pygmsh.helpers.extract_to_meshio()
